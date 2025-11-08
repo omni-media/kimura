feb4a16bf3280b0a5cbafca9fae860b6ed202f7c
Przemek <przemekgg2002@gmail.com>
2025-11-08 13:20:34 +0100

    tweak: demo

--- s/demo/demo.bundle.ts
+++ s/demo/demo.bundle.ts
@@ -17,17 +17,15 @@ document.body.appendChild(app.canvas)
 const a = app.stage.addChild(new Graphics())
 a.circle(0, 0, 100)
     .fill(0xfedbac)
-a.pivot.set(50, 100)
-a.scale.set(1)
-a.position.set(300, 300)
 
 const b = app.stage.addChild(new Graphics())
 b.circle(0, 0, 100)
     .fill(0xfedbac)
-b.pivot.set(50, 100)
-b.scale.set(1)
+
 b.position.set(600, 300)
+a.position.set(300, 300)
 
 app.stage.addChild(new Kimura({
-    group: [a, b],
+    group: [b, a],
 }))
+
